7305b20bdf5a9ff75a61906d040db68deb3233a0
Luis Antonio Castro <luisantoniokoyari@gmail.com>
2024-06-01 18:52:36 +0200

    endpoint

--- src/courses.ts
+++ src/courses.ts
@@ -12,13 +12,32 @@ DELETE  /courses/:id
 */
 
 router.get("/", async (req, res) => {
-    try{
-        const courses = await db.course.findMany();
-        res.status(200).json(courses);
-    } catch (e) {
-        res.status(500).json({ error: "Internal error"})
-    }
-    
-})
+  try {
+    const courses = await db.course.findMany();
+    res.status(200).json(courses);
+  } catch (e) {
+    res.status(500).json({ error: "Internal error" });
+  }
+});
 
-export default router
+router.post("/", async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (name === undefined || typeof name !== "string") {
+      return res.status(400).json({
+        error: "Missing name",
+      });
+    }
+
+    const course = await db.course.create({
+      data: { name },
+    });
+    res.status(201).json(course);
+  } catch (e) {
+    res.status(500).json({
+      error: "Couldn'nt create course. Try again later.",
+    });
+  }
+});
+
+export default router;
