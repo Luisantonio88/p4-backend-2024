4525bbce8f89093aaa7c1701ac3962d782d5b47a
Luis Antonio Castro <luisantoniokoyari@gmail.com>
2024-06-02 09:33:03 +0200

    response

--- src/courses.ts
+++ src/courses.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { db } from "./db";
+import { send } from "./response";
 
 const router = Router();
 
@@ -14,9 +15,24 @@ DELETE  /courses/:id
 router.get("/", async (req, res) => {
   try {
     const courses = await db.course.findMany();
-    res.status(200).json(courses);
+    send(res).ok(courses);
   } catch (e) {
-    res.status(500).json({ error: "Internal error" });
+    send(res).internalError("Could not get forum.");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const course = await db.course.findUniqueOrThrow({
+      where: { courseId: Number(id) },
+    });
+    send(res).ok({ course });
+  } catch (e: any) {
+    if (e.name === "NotFoundError") {
+      return send(res).notFound();
+    }
+    send(res).internalError("Internal Error");
   }
 });
 
@@ -24,19 +40,14 @@ router.post("/", async (req, res) => {
   try {
     const { name } = req.body;
     if (name === undefined || typeof name !== "string") {
-      return res.status(400).json({
-        error: "Missing name",
-      });
+      return send(res).badRequest("Missing name field");
     }
-
     const course = await db.course.create({
       data: { name },
     });
-    res.status(201).json(course);
+    send(res).createOk(course);
   } catch (e) {
-    res.status(500).json({
-      error: "Couldn'nt create course. Try again later.",
-    });
+    send(res).internalError("Couldn't create forum.");
   }
 });
 
